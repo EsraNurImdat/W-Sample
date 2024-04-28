25c673cdc13f950d0ad35e85df10b9203b05f298
zeycandemirdag <zeycandemirdag@hotmail.com>
2024-04-28 16:27:55 +0300

    Search Screen Responsive Page Settings

--- frontend/src/Components/Search.jsx
+++ frontend/src/Components/Search.jsx
@@ -205,6 +205,10 @@ export default function SearchBA() {
   );
 }
 */
+
+
+
+/*
 import React, { useState } from "react";
 import TextField from "@mui/material/TextField";
 import Select from "@mui/material/Select";
@@ -433,6 +437,215 @@ export default function SearchBA() {
             </Alert>
 
           </div>
+        )}
+       
+      </div>
+    </ThemeProvider>
+  );
+}
+*/
+
+
+
+import React, { useState } from "react";
+import TextField from "@mui/material/TextField";
+import Select from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import InputLabel from "@mui/material/InputLabel";
+import Button from '@mui/material/Button';
+import Alert from '@mui/material/Alert';
+import Box from '@mui/material/Box';
+import { blue } from '@mui/material/colors';
+import { useNavigate } from "react-router-dom";
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Stepper from '@mui/material/Stepper';
+import Step from '@mui/material/Step';
+import StepLabel from '@mui/material/StepLabel';
+import LinearProgress from '@mui/material/LinearProgress';
+
+const customTheme = createTheme({
+  palette: {
+    contrastThreshold: 4.5,
+    darker: blue[900],
+  },
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: {
+        body: {
+          backgroundSize: 'cover',
+          minHeight: '1000px',
+        },
+      },
+    },
+  },
+});
+
+const Search = ({ setSearchQuery }) => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "column",
+        marginTop: 10,
+      }}
+    >
+      <TextField
+        id="search-bar"
+        className="text"
+        onInput={(e) => {
+          setSearchQuery(e.target.value);
+        }}
+        label="Enter URL"
+        variant="outlined"
+        placeholder="Search..."
+        size="large"
+        fullWidth
+        style={{ margin: "10px auto", minWidth: 200 }}
+      />
+    </div>
+  );
+};
+
+export default function SearchBA() {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [technique, setTechnique] = useState("");
+  const [error, setError] = useState(null);
+  const [activeStep, setActiveStep] = useState(1); // Track current step
+  const [status, setStatus] = useState("");
+  const navigate = useNavigate();
+
+  const steps = ['Enter URL', 'Select Sampling Technique', 'Crawl and Create Samples'];
+
+  const handleSearch = () => {
+    if (activeStep === 0) {
+      setActiveStep(1);
+    } else if (activeStep === 2) {
+      setActiveStep(2);
+      const axios = require('axios');
+      const data = {
+        technique: technique,
+        searchQuery: searchQuery,
+      };
+      setSearchQuery(searchQuery);
+      if (isMainURL(searchQuery)) {
+        setError(null);
+        setStatus("waiting");
+        fetch("http://127.0.0.1:5000/searchscreen", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ searchQuery: searchQuery , technique: technique }),
+        })
+          .then((response) => response.json())
+          .then((data) => {
+            if (Array.isArray(data) && data.length === 0) {
+              setStatus("error")
+              setError("Failed to crawl!")
+          } else {
+              if (data.hasOwnProperty("message")) {
+                  setStatus("error");
+                  setError(data.message)
+              } else {
+                  setStatus("done");
+                  setActiveStep(3)
+                  navigate('/resultscreen');
+              } }
+            
+          })
+          .catch((error) => {
+            console.error("Error occurred:", error);
+            setStatus("error");
+            setError(error)
+          });
+      } else {
+        setStatus("error")
+        setError("Error: Not a main URL");
+      }
+    }
+  };
+
+  const handleChange = (event) => {
+    setTechnique(event.target.value);
+    setActiveStep(2);
+  };
+
+  const isMainURL = (url) => {
+    try {
+      const parsedUrl = new URL(url);
+      return parsedUrl.pathname === "/" && parsedUrl.search === "";
+    } catch (error) {
+      // Check invalid URLs 
+      return false;
+    }
+  };
+
+  return (
+    <ThemeProvider theme={customTheme}>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          flexDirection: "column",
+          minHeight: "70vh",
+        }}
+      >
+          <Stepper activeStep={activeStep}>
+          {steps.map((label) => (
+            <Step key={label}>
+              <StepLabel>{label}</StepLabel>
+            </Step>
+          ))}
+        </Stepper>
+        <Search setSearchQuery={setSearchQuery} />
+        <br></br>
+        {activeStep === 1 && (
+          <FormControl sx={{ minWidth: "80%", maxWidth: "80%", marginBottom: 2 }}>
+            <InputLabel>Select Sampling Technique</InputLabel>
+            <Select
+              labelId="tech"
+              id="tech" 
+              label="Select Sampling Technique"
+              value={technique}
+              onChange={handleChange}
+            > 
+              <MenuItem value={"a"}>(a) the home, login, sitemap, contact, help and legal information pages</MenuItem> 
+              <MenuItem value={"c"}>(c) the pages containing the accessibility statement or policy and the pages containing the feedback mechanism</MenuItem>
+              <MenuItem value={"d"}>(d) examples of pages having a substantially distinct appearance or presenting a different type of content</MenuItem>
+              <MenuItem value={"g"}>(g) randomly selected pages amounting to at least 10 % of the sample established by points (a) to (f)…</MenuItem>
+            </Select>
+          </FormControl>
+        )}
+        {activeStep === 2 &&  status != "waiting" && (
+       <Button
+       type="submit"
+       variant="contained"
+       sx={{ mt: 2 }}
+       onClick={handleSearch}
+     >
+       Crawl
+     </Button>
+        )}
+         
+         {status === "waiting" && (
+          <Box sx={{ width: "80%", mt: 2 }}>
+            <LinearProgress />
+          </Box>
+        )}
+    {status === "done" && (
+          <Alert severity="success" sx={{ width: "80%", mt: 2 }}>
+            Crawling completed!
+          </Alert>
+        )}
+
+        {status === "error" && (
+          <Alert severity="error" sx={{ width: "80%", mt: 2 }}>
+            {error}
+          </Alert>
         )}
        
       </div>
--- frontend/src/Screeens/SearchScreen.jsx
+++ frontend/src/Screeens/SearchScreen.jsx
@@ -1,4 +1,4 @@
-import Navigator from "../Components/Navigator";
+/*import Navigator from "../Components/Navigator";
 import Sidebar from "../Components/SideBar";
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
@@ -70,4 +70,74 @@ function SearchScreen ()  {
     </ThemeProvider>
   );
 };
+export default SearchScreen;
+*/
+
+import Navigator from "../Components/Navigator";
+import Sidebar from "../Components/SideBar";
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import { Typography } from "@mui/material";
+import SearchBA from "../Components/Search";
+import Wlogo from "../assets/Wlogo.png";
+
+const customTheme = createTheme({
+  palette: {},
+  primary: {
+    main: '#01579b',
+  },
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: {
+        body: {
+          backgroundImage: 'url("https://i.pinimg.com/564x/4f/d4/97/4fd4972cb3cf77fda159e722cf1fc6ac.jpg")',
+          backgroundSize: 'cover',
+          minHeight: '1000px',
+        },
+      },
+    },
+  },
+});
+
+function SearchScreen() {
+  return (
+    <ThemeProvider theme={customTheme}>
+      <Box sx={{ display: 'flex' }}>
+        <CssBaseline />
+        <Navigator />
+        <Sidebar />
+        <Box
+          component="main"
+          sx={{
+            flexGrow: 1,
+            height: '100vh',
+            overflow: 'auto',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center', // Center align content
+          }}
+        >
+          <Toolbar />
+          <Box sx={{ flex: 1, textAlign: 'center' }}>
+            <img
+              src={Wlogo}
+              alt="Logo"
+              style={{
+                marginTop: "100px", // Boşluğu azalttık
+                width: "70%", // Yüzde cinsinden boyutlandırma
+                height: "auto",
+                maxWidth: "700px", // Maksimum genişlik belirlendi
+                marginBottom: -180,
+              }}
+            />
+            <SearchBA />
+          </Box>
+        </Box>
+      </Box>
+    </ThemeProvider>
+  );
+}
+
 export default SearchScreen;
